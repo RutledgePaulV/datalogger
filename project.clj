ea5a7bba6719129bf567398e7ebbcc20edcb28a4
Paul Rutledge <rutledgepaulv@gmail.com>
2021-10-31 21:53:25 -0500

    Version 0.1.16

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.rutledgepaulv/datalogger "0.1.16-SNAPSHOT"
+(defproject org.clojars.rutledgepaulv/datalogger "0.1.16"
 
   :description
   "A simple structured logging library for Clojure that plays nice with others."
