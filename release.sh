b7b3efbde4e87576ef55a49ec5fe0515bfb83bfc
Paul Rutledge <rutledgepaulv@gmail.com>
2023-10-18 01:16:23 -0500

    update release script

--- release.sh
+++ release.sh
@@ -2,6 +2,7 @@
 
 clj -X:build clean
 clj -X:build jar
+version=$(clj -X:build get-version)
 
 export CLOJARS_USERNAME="op://Personal/clojars.org/username"
 export CLOJARS_PASSWORD="op://Personal/clojars.org/token"
@@ -9,8 +10,11 @@ export CLOJARS_PASSWORD="op://Personal/clojars.org/token"
 op run -- mvn deploy:deploy-file \
   -DgroupId="org.clojars.rutledgepaulv" \
   -DartifactId="datalogger" \
-  -Dversion="$(clj -X:build get-version)" \
+  -Dversion="$version" \
   -Dpackaging="jar" \
   -Dfile="target/datalogger.jar" \
   -DrepositoryId="clojars" \
   -Durl="https://repo.clojars.org"
+
+git tag "v$version"
+git push origin "refs/tags/v$version"
