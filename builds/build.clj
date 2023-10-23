a4700830f811011d998df76fe5e08444b9be345b
Paul Rutledge <rutledgepaulv@gmail.com>
2023-10-22 22:29:45 -0500

    always print the logging config as part of first message

--- builds/build.clj
+++ builds/build.clj
@@ -2,7 +2,7 @@
   (:require [clojure.tools.build.api :as b]))
 
 (def lib 'org.clojars.rutledgepaulv/datalogger)
-(def version "2.0.0")
+(def version "2.0.1")
 (def class-dir "target/classes")
 (def basis (b/create-basis {:project "deps.edn"}))
 (def jar-file (format "target/datalogger.jar" (name lib) version))
